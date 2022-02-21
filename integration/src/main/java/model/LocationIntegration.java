71442e7a8b2e81b64e3631f81ab68eb05246916e
Leonardo1364 <leo_lmota@hotmail.com>
2022-02-21 15:45:13 -0300

    implemented webclient

---
+++ integration/src/main/java/model/LocationIntegration.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LocationIntegration {
+
+    private String locale;
+    private String name;
+}
