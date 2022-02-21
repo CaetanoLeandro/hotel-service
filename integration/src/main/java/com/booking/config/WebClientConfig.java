9276a0b35cdcf6528c4c6f1ffa45bd3da2910598
Marcos Silveira <marcosrogerio1994@gmail.com>
2022-02-21 15:33:57 -0300

    WebClient Config

---
+++ integration/src/main/java/com/booking/config/WebClientConfig.java
@@ -0,0 +1,22 @@
+package com.booking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .baseUrl("https://booking-com.p.rapidapi.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("x-rapidapi-host, booking-com.p.rapidapi.com")
+                .defaultHeader("x-rapidapi-key", "ecf770b382mshc04ae62c93fbcc3p194808jsnf3dfb855b376")
+                .build();
+    }
+
+}
