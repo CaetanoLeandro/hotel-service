7aba5ed568509a0722e684b477a4ab2c6faee583
Marcos Silveira <marcosrogerio1994@gmail.com>
2022-02-22 12:33:30 -0300

    Reslvendo packages

--- integration/src/main/java/com/booking/config/model/LocationIntegration.java
+++ integration/src/main/java/com/booking/config/model/LocationIntegration.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:integration/src/main/java/com/booking/config/model/LocationIntegration.java
 package com.booking.config.model;
-=======
-package com.booking.location.model.request;
->>>>>>> ce0cbc6... atualizou as classes:integration/src/main/java/com/booking/location/model/request/LocationIntegrationRequest.java
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- integration/src/main/java/com/booking/location/model/request/LocationIntegrationRequest.java
+++ integration/src/main/java/com/booking/location/model/request/LocationIntegrationRequest.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:integration/src/main/java/com/booking/config/model/LocationIntegration.java
-package com.booking.config.model;
-=======
 package com.booking.location.model.request;
->>>>>>> ce0cbc6... atualizou as classes:integration/src/main/java/com/booking/location/model/request/LocationIntegrationRequest.java
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class LocationIntegration {
-
+public class LocationIntegrationRequest {
     private String locale;
     private String name;
+
 }
