0bc74220f1667d92f82657fef0aa71f63fd1002f
Matheus Schio <matheus.schiod@gmail.com>
2022-02-23 14:26:32 -0300

    arrumando dependencias swagger

--- contract/src/main/java/com/booking/controller/v1/location/LocationController.java
+++ contract/src/main/java/com/booking/controller/v1/location/LocationController.java
@@ -43,5 +43,4 @@ public class LocationController {
                         .locale(locale)
                         .build());
     }
-    @GetMapping()
 }
--- integration/src/main/java/com/booking/config/SwaggerConfig.java
+++ integration/src/main/java/com/booking/config/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.booking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -16,7 +14,7 @@ import static springfox.documentation.builders.PathSelectors.any;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebFluxConfigurationSupport {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -39,15 +37,6 @@ public class SwaggerConfig extends WebFluxConfigurationSupport {
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .build();
-    }
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
 
